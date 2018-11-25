a65611da522cb8ff0d666c7c0a786a2b695c67b8
sjoseantogt <joseantogta@gmail.com>
2018-11-25 17:31:55 -0600

    Commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CardContainer from './components/js/card-container'
+
+ReactDOM.render(
+    <CardContainer/>, 
+    document.getElementById('root')
+    )
+
