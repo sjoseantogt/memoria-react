f3cfaa559fc1780933f9969900fd9b8bf4f995ae
sjoseantogt <44790675+sjoseantogt@users.noreply.github.com>
2018-11-25 17:36:23 -0600

    Delete index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import CardContainer from './components/js/card-container'
-
-ReactDOM.render(
-    <CardContainer/>, 
-    document.getElementById('root')
-    )
-
